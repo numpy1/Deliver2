01eaab933da59dba31c6d5560a4b367a8dbc18fd
numpy1 <business.maerz@gmail.com>
2016-04-05 12:00:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marvin/deliver2/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.marvin.deliver2.app;
+
+/**
+ * Created by marvin on 19.03.16.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://deliver.lima-city.de/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://deliver.lima-city.de/register.php";
+}
